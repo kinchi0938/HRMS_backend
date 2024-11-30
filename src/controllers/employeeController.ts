151c495937b51ffebebf79a7fdf156beca44c4e2
KinChi <kinchi0938@gmail.com>
2024-11-30 14:10:04 +0900

    feat(employeeController): add signup

--- src/controllers/employeeController.ts
+++ src/controllers/employeeController.ts
@@ -14,3 +14,85 @@ export const employeeList = async (req: Request, res: Response) => {
     });
   }
 };
+
+/** create a new Employee */
+export const Signup = async (req: Request, res: Response) => {
+  const {
+    username,
+    password,
+    password2,
+    firstName,
+    lastName,
+    email,
+    street,
+    housenumber,
+    zipcode,
+    city,
+    country,
+    role,
+    admin,
+  } = req.body;
+  console.log(req.body);
+  // check all the required fields
+  if (
+    !username ||
+    !password ||
+    !password2 ||
+    !firstName ||
+    !lastName ||
+    !email ||
+    !role
+  ) {
+    return res
+      .status(403)
+      .json({ errorMessage: "Please Check required fields" });
+  }
+  // Validate the email
+  if (!ValidateEmail(email)) {
+    return res.status(403).json({ errorMessage: "Invalid email address." });
+  }
+
+  if (password !== password2) {
+    return res
+      .status(403)
+      .json({ errorMessage: "Password confirmation does not match." });
+  }
+  // check if there is a employee with same username
+  const usernameExists = await Employee.exists({ username });
+  if (usernameExists) {
+    return res.status(403).json({
+      errorMessage: "This username is already taken.",
+    });
+  }
+  // check if there is a employee with same email
+  const emailExists = await Employee.exists({ email });
+  if (emailExists) {
+    return res.status(403).json({
+      errorMessage: "This email is already taken.",
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 5);
+
+  try {
+    const createdUser = await Employee.create({
+      username,
+      password: hashedPassword,
+      firstName,
+      lastName,
+      email,
+      street,
+      housenumber,
+      zipcode,
+      city,
+      country,
+      role,
+      admin,
+    });
+    res.status(200).json({ createdUser });
+  } catch (error) {
+    return res.status(400).json({
+      errorMessage: error,
+    });
+  }
+};
--- src/router/rootRouter.ts
+++ src/router/rootRouter.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import { Signup } from "../controllers/employeeController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/");
-rootRouter.post("/signup");
+rootRouter.post("/signup", Signup);
 rootRouter.post("/login");
 
 export default rootRouter;
