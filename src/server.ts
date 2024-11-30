872dc5085953e4d96d2763430f72d7291e5f4504
KinChi <kinchi0938@gmail.com>
2024-11-30 10:51:40 +0900

    feat : express server setup

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const logger = morgan("dev");
+
+app.use(cors());
+
+app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 4000;
+
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
